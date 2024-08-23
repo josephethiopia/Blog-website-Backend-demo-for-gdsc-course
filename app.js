bf3c6e7e5e29ba4787d381cab100d679c078eb4b
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-08-23 19:32:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,38 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blog-routes.js";
+
 const app = express();
+
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect("mongodb+srv://admin:admin.0596@moti.fhkz5xa.mongodb.net/?retryWrites=true&w=majority&appName=Moti").then(()=>app.listen(5000)).then(()=>console.log("connected to DB")).catch((err)=>console.log(err));
-// then is used here bacauuse mongoose.connect will give a promise so to solve that we use then here  
+// MongoDB connection URI
+const MONGO_URI =
+  "mongodb+srv://admin:admin.0596@moti.fhkz5xa.mongodb.net/?retryWrites=true&w=majority&appName=Moti";
+
+// Function to start the server
+const startServer = () => {
+  app.listen(5000, () => {
+    console.log("The server is ready to serve on port 5000");
+  });
+};
+
+// Connect to MongoDB and start the server
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    startServer();
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1); // Exit the process with a failure code
+  });
